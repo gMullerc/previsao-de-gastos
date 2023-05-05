01de84df93949f92a559102caf8e15ca0c667e5d
Guilherme Muller <gmuller@magnasistemas.com.br>
2023-05-05 20:54:00 -0300

    Nova atualização, previsao mais acertiva

--- src/main/java/br/com/uninove/previsao.java
+++ src/main/java/br/com/uninove/previsao.java
@@ -1,87 +1,63 @@
 package br.com.uninove;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-
+import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
-import weka.classifiers.functions.LinearRegression;
-import weka.core.DenseInstance;
-import weka.core.Instances;
-
 public class previsao {
+    public static void main(String[] args) {
+        // Dados históricos de lucro
+        double[] meses = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12};
+        double[] lucros = {3000, 3200, 3300, 2100, 2100, 1600, 1231, 1000, 200,158, 133,150};
 
-	public static void main(String[] args) throws Exception {
+        // Criar objeto de regressão linear
+        SimpleRegression regression = new SimpleRegression();
+        for (int i = 0; i < meses.length; i++) {
+            regression.addData(meses[i], lucros[i]);
+        }
 
+        // Previsão de lucro para os próximos meses
+        int mesesFuturos = 3;
+        double ultimoMes = meses[meses.length - 1];
+        double[] lucrosPrevistos = new double[mesesFuturos];
+        for (int i = 0; i < mesesFuturos; i++) {
+            lucrosPrevistos[i] = regression.predict(ultimoMes + i + 1);
+        }
 
-		XYSeries predicted = new XYSeries("Gastos previstos");
+        // Criar dataset com os dados históricos e previstos
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        XYSeries historicoSeries = new XYSeries("Histórico de Lucro");
+        XYSeries previsaoSeries = new XYSeries("Previsão de Lucro");
 
-		// Carrega o arquivo CSV
-		BufferedReader reader = new BufferedReader(new FileReader("dados.ARFF"));
-		Instances data = new Instances(reader);
-		reader.close();
+        for (int i = 0; i < meses.length; i++) {
+            historicoSeries.add(meses[i], lucros[i]);
+        }
 
-		
-		XYSeries actual = new XYSeries("Gastos reais");
-		actual.add(1, 1500.0);
-		actual.add(2, 1800.0);
-		actual.add(3, 10100.0);
-		actual.add(4, 2200.0);
-		actual.add(5, 2500.0);
-		actual.add(6, 2800.0);
-		actual.add(7, 3000.0);
-		actual.add(8, 3300.0);
-		actual.add(9, 3600.0);
-		actual.add(10, 4000.0);
-		actual.add(11, 4500.0);
-		actual.add(12, 5000.0);
+        for (int i = 0; i < mesesFuturos; i++) {
+            previsaoSeries.add(ultimoMes + i + 1, lucrosPrevistos[i]);
+        }
 
-		// Especifica a variável dependente (gastos) e as variáveis independentes
-		// (características)
-		data.setClassIndex(data.numAttributes() - 1);
+        dataset.addSeries(historicoSeries);
+        dataset.addSeries(previsaoSeries);
 
-		// Treina o modelo de regressão linear
-		LinearRegression model = new LinearRegression();
-		model.buildClassifier(data);
+        // Criar gráfico de linha
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Previsão de Lucro", // Título do gráfico
+                "Mês", // Título do eixo X
+                "Lucro", // Título do eixo Y
+                dataset, // Dados do gráfico
+                PlotOrientation.VERTICAL, // Orientação do gráfico
+                true, // Exibir legenda
+                true, // Exibir tooltips
+                false // Não exibir URLs
+        );
 
-		for (int i = 1; i <= 12; i++) {
-			double[] valores = { 2023, i }; // Exemplo para previsão dos gastos de cada mês de 2022
-
-			// Faz a previsão dos gastos para o mês desejado
-			double prev_gastos = model.classifyInstance(new DenseInstance(1.0, valores));
-
-			// Adiciona o valor previsto ao gráfico
-			predicted.add(i, prev_gastos);
-		}
-
-		// Define os valores das características para o ano desejado
-		double[] valores = { 2023, 5 }; // Exemplo para previsão dos gastos de janeiro de 2022
-
-		// Cria uma nova instância para teste
-		DenseInstance instance = new DenseInstance(1.0, valores);
-		instance.setDataset(data);
-
-		// Faz a previsão dos gastos para o ano desejado
-		double prev_gastos = model.classifyInstance(instance);
-
-		// Imprime o resultado da previsão
-		System.out.println("Previsão de gastos para o mês 5 do ano 2023: " + prev_gastos);
-		XYSeriesCollection dataset = new XYSeriesCollection();
-		dataset.addSeries(actual);
-		dataset.addSeries(predicted);
-
-		// Cria o gráfico
-		ChartFrame frame = new ChartFrame("Previsão de Gastos para 2023", ChartFactory.createXYLineChart(
-		    "Previsão de Gastos para 2023", "Mês", "Gastos", dataset, PlotOrientation.VERTICAL,
-		    true, true, false));
-
-		// Exibe o gráfico
-		frame.pack();
-		frame.setVisible(true);
-	}
-
+        // Exibir gráfico em uma janela
+        ChartFrame frame = new ChartFrame("Previsão de Lucro", chart);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
