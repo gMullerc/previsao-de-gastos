428ed0ec7e4bf4b643229a599edbb60856d16d93
Guilherme Muller <gmuller@magnasistemas.com.br>
2023-05-19 18:51:17 -0300

    Alteracao para entrada de dados do usuario e conta feita na mao como solicitado

--- src/main/java/br/com/uninove/previsao.java
+++ src/main/java/br/com/uninove/previsao.java
@@ -1,5 +1,7 @@
 package br.com.uninove;
-import org.apache.commons.math3.stat.regression.SimpleRegression;
+
+import java.util.Scanner;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
@@ -9,22 +11,53 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 public class previsao {
     public static void main(String[] args) {
-        // Dados históricos de lucro
-        double[] meses = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12};
-        double[] lucros = {3000, 3200, 3300, 2100, 2100, 1600, 1231, 1000, 200,158, 133,150};
+    	
+        Scanner scanner = new Scanner(System.in);
 
-        // Criar objeto de regressão linear
-        SimpleRegression regression = new SimpleRegression();
-        for (int i = 0; i < meses.length; i++) {
-            regression.addData(meses[i], lucros[i]);
+        System.out.print("Digite a quantidade de meses: ");
+        int quantidadeMeses = scanner.nextInt();
+
+        double[] meses = new double[quantidadeMeses];
+        double[] lucros = new double[quantidadeMeses];
+
+        for (int i = 0; i < quantidadeMeses; i++) {
+            System.out.print("Digite o lucro do mês " + (i + 1) + ": ");
+            lucros[i] = scanner.nextDouble();
+            meses[i] = i + 1;
         }
 
-        // Previsão de lucro para os próximos meses
-        int mesesFuturos = 3;
-        double ultimoMes = meses[meses.length - 1];
+      
+        double n = meses.length;
+        double sumX = 0;
+        double sumY = 0;
+        double sumXY = 0;
+        double sumX2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            sumX += meses[i];
+            sumY += lucros[i];
+            sumXY += meses[i] * lucros[i];
+            sumX2 += meses[i] * meses[i];
+        }
+
+        double mediaX = sumX / n;
+        double mediaY = sumY / n;
+
+        double b1 = (sumXY - (n * mediaX * mediaY)) / (sumX2 - (n * mediaX * mediaX));
+        double b0 = mediaY - (b1 * mediaX);
+
+
+
+        System.out.print("Digite a quantidade de meses que deseja prever com seus dados: ");
+        int mesesFuturos = scanner.nextInt();
+        
+        double ultimoMes = meses[(int) (n - 1)];
         double[] lucrosPrevistos = new double[mesesFuturos];
+
         for (int i = 0; i < mesesFuturos; i++) {
-            lucrosPrevistos[i] = regression.predict(ultimoMes + i + 1);
+            double mes = ultimoMes + i + 1;
+            double lucroPrevisto = b0 + (b1 * mes);
+            lucrosPrevistos[i] = lucroPrevisto;
         }
 
         // Criar dataset com os dados históricos e previstos
@@ -45,14 +78,14 @@ public class previsao {
 
         // Criar gráfico de linha
         JFreeChart chart = ChartFactory.createXYLineChart(
-                "Previsão de Lucro", // Título do gráfico
-                "Mês", // Título do eixo X
-                "Lucro", // Título do eixo Y
-                dataset, // Dados do gráfico
-                PlotOrientation.VERTICAL, // Orientação do gráfico
-                true, // Exibir legenda
-                true, // Exibir tooltips
-                false // Não exibir URLs
+                "Previsão de Lucro", 
+                "Mês", 
+                "Lucro", 
+                dataset, 
+                PlotOrientation.VERTICAL,
+                true, 
+                true, 
+                false 
         );
 
         // Exibir gráfico em uma janela
